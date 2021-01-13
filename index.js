b2e511bdd1f9511896a3e631218632c1154a6512
冯然峰 <fengranfeng01@inke.cn>
2021-01-13 11:52:52 +0800

    Updates

---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+var React = require('react');
+
+function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'default' in e ? e : { 'default': e }; }
+
+var React__default = /*#__PURE__*/_interopDefaultLegacy(React);
+
+var index = (function (_ref) {
+  var title = _ref.title;
+  return /*#__PURE__*/React__default['default'].createElement("button", null, "\u6211\u662Fantd\u7684\u6309\u94AE");
+});
+
+var index$1 = (function (_ref) {
+  var title = _ref.title;
+  return /*#__PURE__*/React__default['default'].createElement("button", null, "\u6211\u662Fantd\u7684\u6309\u94AE1");
+});
+
+exports.MyButton = index;
+exports.MyButton1 = index$1;
